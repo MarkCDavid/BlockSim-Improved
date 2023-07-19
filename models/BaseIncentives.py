06fefaf7a0693f39c649fee47e09d0c67d505b26
Aurimas Šakalys <aurimas.sakalys@stud.vilniustech.lt>
2023-07-19 23:03:51 +0300

    BaseIncentives

---
+++ models/BaseIncentives.py
@@ -0,0 +1,20 @@
+import configuration
+from models.BaseBlock import BaseBlock
+from models.BaseConsensus import BaseConsensus
+
+
+class Incentives:
+
+    def distribute_rewards(self: 'Incentives', consensus: 'BaseConsensus'):
+        for block in consensus.global_chain:
+            for node in configuration.NODES:
+                if block.miner != node.id:
+                    continue
+
+                node.blocks += 1
+                node.balance += configuration.BLOCK_REWARD
+                node.balance += self.transactions_fee(block)
+
+    # FIXME: Move to the BaseBlock
+    def transactions_fee(self: 'Incentives', block: 'BaseBlock') -> 'float':
+        return sum(transaction.fee for transaction in block.transactions)
